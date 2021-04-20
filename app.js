097355e892a4ee4b7e0f77202e11b718b7cccaf8
Ryan Snodgrass <rsnodgra92@gmail.com>
2021-04-20 12:33:12 -0600

    Express Session Part 1

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require("express-session")
+//calls the require function which returns a function, which is then passed the session argument and run
+const FileStore= require("session-file-store")(session)
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -36,13 +39,28 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //pass in a secret key here, it could be any string
-app.use(cookieParser("secretkey"));
+//app.use(cookieParser("secretkey")); Express session offers its own implementation of cookies.
+//using both can cause conflicts so we will not use cookieParser
+app.use(session(
+  {
+    name: "session-id", 
+    secret: "secretkey", 
+    //saveUnintialized when a new session is created but no updates are made to it, at the end of the req it wont get saved. No cookie is sent to the client, this is to prevent having a bunch of empty session files and cookies from being set up. 
+    saveUninitialized: false, 
+    resave: false, 
+    //save to hard disk not just running application memory
+    store: new FileStore()
+  }
+))
 
 //middle ware order matters, we put authentication check here if req does not pass, they will not move on to the rest
 function auth(req,res,next)
 {
+  //the session middleware will add a prop called session to the request object 
+  console.log(req.session);
+
   //the singedCookies prop is provided by the cookie parser middleware
-  if(!req.signedCookies.user)
+  if(!req.session.user)
   {
       const authHeader = req.headers.authorization
     
@@ -63,8 +81,8 @@ function auth(req,res,next)
     
       if (user === "admin" && pass === "password")
       {
-        //create a new cookie if the user is successfully validated
-        res.cookie("user", "admin", {signed: true})
+        //create a new cookie/session if the user is successfully validated
+        req.session.user = "admin"
         //now the user has be authorized
         return next()
       }
@@ -81,7 +99,7 @@ function auth(req,res,next)
 
   else
   {
-    if(req.signedCookies.user === "admin")
+    if(req.session.user === "admin")
     {
       return next()
     }
