ba90bd2dde7a282c04464e0685cf450aaea7113a
Ryan Snodgrass <rsnodgra92@gmail.com>
2021-04-22 13:40:32 -0600

    Mongoose Population

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const User= require("../models/user")
 const passport = require("passport")
-const authenticate = require("../authenticate")
+const authenticate = require("../authenticate");
+const user = require('../models/user');
 
 const router = express.Router();
 
@@ -11,31 +12,39 @@ router.get('/', function(req, res, next)
   res.send('respond with a resource');
 });
 
-router.post("/signup", (req,res) =>{
- 
+router.post('/signup', (req, res) => {
   User.register(
-    new User({username: req.body.username}), 
-    req.body.password, 
-    err =>{
-     
-      if(err)
-      {
-         res.statusCode= 500
-         res.setHeader("Content-Type","application/json")
-         res.json({err:err})
+      new User({username: req.body.username}),
+      req.body.password,
+      (err, user) => {
+          if (err) {
+              res.statusCode = 500;
+              res.setHeader('Content-Type', 'application/json');
+              res.json({err: err});
+          } else {
+              if (req.body.firstname) {
+                  user.firstname = req.body.firstname;
+              }
+              if (req.body.lastname) {
+                  user.lastname = req.body.lastname;
+              }
+              user.save(err => {
+                  if (err) {
+                      res.statusCode = 500;
+                      res.setHeader('Content-Type', 'application/json');
+                      res.json({err: err});
+                      return;
+                  }
+                  passport.authenticate('local')(req, res, () => {
+                      res.statusCode = 200;
+                      res.setHeader('Content-Type', 'application/json');
+                      res.json({success: true, status: 'Registration Successful!'});
+                  });
+              });
+          }
       }
-      
-      else
-      {
-         passport.authenticate("local")(req, res, ()=>{
-            res.statusCode = 200
-            res.setHeader("Content-Type", "application/json")
-            res.json({success: true, status: "Regisration Successful!"})
-         })
-      }
-    }
-  )
-})
+  );
+});
 
 //passport.authenticate() will handle logging in the user for us from now on including challenging the user for credentials, and parsing the credentials from the request body
 router.post('/login',passport.authenticate("local"),(req, res) => {
