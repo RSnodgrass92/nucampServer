42562a18032dfa3271455ec7c37be81c0231dcea
Ryan Snodgrass <rsnodgra92@gmail.com>
2021-04-24 09:49:58 -0600

    task 2

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -23,7 +23,7 @@ campsiteRouter.route("/")
     .catch(err => next(err))
 })
 
-.post(authenticate.verifyUser,(req,res, next)=>
+.post(authenticate.verifyUser,authenticate.verifyAdmin,(req,res, next)=>
 {
     //mongoose will already check this to make sure it matches the schema we defined
     Campsite.create(req.body)
@@ -42,7 +42,7 @@ campsiteRouter.route("/")
     res.end("PUT operation not supported on /campsites")
 })
 
-.delete(authenticate.verifyUser,(req,res, next)=>{
+.delete(authenticate.verifyUser,authenticate.verifyAdmin,(req,res, next)=>{
     Campsite.deleteMany()
     .then(response => {
         res.statusCode= 200
@@ -72,7 +72,7 @@ campsiteRouter.route(`/:campsiteId`)
     res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`)
 })
 
-.put(authenticate.verifyUser,(req,res, next)=>{
+.put(authenticate.verifyUser,authenticate.verifyAdmin,(req,res, next)=>{
     Campsite.findByIdAndUpdate(req.params.campsiteId, {$set: req.body}, {new: true})
     .then(campsite =>{
         res.statusCode = 200
@@ -82,7 +82,7 @@ campsiteRouter.route(`/:campsiteId`)
     .catch(err => next(err))
 })
 
-.delete(authenticate.verifyUser,(req,res, next)=>{
+.delete(authenticate.verifyUser,authenticate.verifyAdmin,(req,res, next)=>{
     Campsite.findByIdAndDelete(req.params.campsiteId)
     .then(response =>{
         res.statusCode = 200
@@ -136,7 +136,7 @@ campsiteRouter.route('/:campsiteId/comments')
     res.statusCode = 403;
     res.end(`PUT operation not supported on /campsites/${req.params.campsiteId}/comments`);
 })
-.delete(authenticate.verifyUser,(req, res, next) => {
+.delete(authenticate.verifyUser,authenticate.verifyAdmin,(req, res, next) => {
     Campsite.findById(req.params.campsiteId)
     .then(campsite => {
         if (campsite) {
